06c564dc57c5ba9ce83aa26b1cd67f858918e04a
K <kihyuk.sung@outlook.kr>
2023-07-01 21:10:14 +0900

    ResponseHeaderServlet
    - response header 설정 방법
    - header / cookie / redirect

---
+++ src/main/kotlin/hello/servlet/basic/response/ResponseHeaderServlet.kt
@@ -0,0 +1,43 @@
+package hello.servlet.basic.response
+
+import jakarta.servlet.annotation.WebServlet
+import jakarta.servlet.http.Cookie
+import jakarta.servlet.http.HttpServlet
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import java.io.InputStream
+
+@WebServlet(name = "responseHeaderServlet", urlPatterns = ["/response-header"])
+class ResponseHeaderServlet : HttpServlet() {
+    override fun service(request: HttpServletRequest, response: HttpServletResponse): Unit = with(response) {
+        status = HttpServletResponse.SC_OK
+
+        content()
+
+        setHeader("Cache-Control", "no-cache, no-store, must-revalidate")
+        setHeader("Pragma", "no-cache")
+        setHeader("my-header", "hello")
+
+        cookie()
+        redirect()
+
+        writer.write("안녕하세요")
+    }
+
+    private fun HttpServletResponse.content() {
+        contentType = "text/plain"
+        characterEncoding = "utf-8"
+    }
+
+    private fun HttpServletResponse.cookie(): Unit =
+        Cookie("myCookie", "good")
+            .apply { maxAge = 600 }
+            .let(this::addCookie)
+
+    private fun HttpServletResponse.redirect() {
+//        status = HttpServletResponse.SC_FOUND
+//        setHeader("Location", "/basic/hello-form.html")
+        sendRedirect("/basic/hello-form.html")
+    }
+
+}
