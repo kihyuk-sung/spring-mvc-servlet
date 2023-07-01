a4e6c48b49836180d7510f65fdb36ef5ade401ed
K <kihyuk.sung@outlook.kr>
2023-07-01 21:33:05 +0900

    ResponseJsonServlet
    - json 전송
    - objectMapper 활용
    - application/json 의 encoding은 반드시 unicode
    - utf-8이 기본값
    - charset은 의미 없음

---
+++ src/main/kotlin/hello/servlet/basic/response/ResponseJsonServlet.kt
@@ -0,0 +1,26 @@
+package hello.servlet.basic.response
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import hello.servlet.basic.HelloData
+import jakarta.servlet.annotation.WebServlet
+import jakarta.servlet.http.Cookie
+import jakarta.servlet.http.HttpServlet
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import java.io.InputStream
+
+@WebServlet(name = "responseJsonServlet", urlPatterns = ["/response-json"])
+class ResponseJsonServlet : HttpServlet() {
+    private val objectMapper = ObjectMapper()
+        .registerKotlinModule()
+
+    override fun service(request: HttpServletRequest, response: HttpServletResponse): Unit = with(response) {
+        contentType = "application/json"
+
+        HelloData(username = "kim", age = 20)
+            .let(objectMapper::writeValueAsString)
+            .let(writer::write)
+    }
+
+}
