e6c6b6df5767dabae77d5bf49d0b1b1dc4e68060
K <kihyuk.sung@outlook.kr>
2023-07-02 20:37:04 +0900

    Add kotest Dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,8 @@ plugins {
 group = "hello"
 version = "0.0.1-SNAPSHOT"
 
+val kotestVersion by extra("5.6.2")
+
 java {
 	sourceCompatibility = JavaVersion.VERSION_17
 }
@@ -25,6 +27,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	providedRuntime("org.springframework.boot:spring-boot-starter-tomcat")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
 }
 
 tasks.withType<KotlinCompile> {
