eff3d9dedd1410c08b1efd33c0e5af24a5abc49f
K <kihyuk.sung@outlook.kr>
2023-07-05 21:45:16 +0900

    SpringMVC RequestMapping V3
    - Controller method의 인자로 다양한 것들을 받을 수 있다.

---
+++ src/main/kotlin/hello/servlet/web/springmvc/v3/SpringMemberControllerV2.kt
@@ -0,0 +1,40 @@
+package hello.servlet.web.springmvc.v3
+
+import hello.servlet.domain.member.IdGenerator
+import hello.servlet.domain.member.Member
+import hello.servlet.domain.member.MemberRepository
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.servlet.ModelAndView
+
+@Controller
+@RequestMapping("/springmvc/v3/members")
+class SpringMemberControllerV3 {
+    @GetMapping("/new-form")
+    fun newForm(): String = "new-form"
+
+    @GetMapping
+    fun members(model: Model): String = model
+        .addAttribute("members", MemberRepository.findAll())
+        .let { "members" }
+
+    @PostMapping("/save")
+    fun save(
+        @RequestParam("username") username: String,
+        @RequestParam("age") age: Int,
+        model: Model,
+    ): String =
+        Member(
+            id = IdGenerator.next(),
+            username = username,
+            age = age,
+        )
+            .let(MemberRepository::save)
+            .let { model.addAttribute("member", it) }
+            .let { "save-result" }
+}
